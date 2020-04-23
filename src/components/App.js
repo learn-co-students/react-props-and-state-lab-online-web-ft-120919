5cfd63051158f762fbdd70dae7a8aac09c44acb4
github username <meliajm@gmail.com>
2020-04-22 19:48:38 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,36 @@ class App extends React.Component {
     }
   }
 
+
+  // not sure about this part, how to set state.pets to result of fetch???
+  // setPetsFromFetch = () => {
+  //   this.setState({
+  //     pets: handleFetch()
+  //   })
+  // }
+
+
+  handleFetch = () => {
+    if (this.state.filters.type === 'cat'){
+      fetch('/api/pets?type=cat')
+    } else if (this.state.filters.type === 'dog') {
+      fetch('/api/pets?type=dog')
+    } else if (this.state.filters.type === 'micropig') {
+      fetch('/api/pets?type=micropig')
+    } else {
+      fetch('/api/pets')
+    }
+  }
+
+  // need to find first not just check equality for each pets????
+  isAdoptedFunct = (petId) => {
+    if (this.state.pets.id===petId) {
+      this.state.pets.isAdopted=true
+    }
+
+
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +54,15 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters 
+              onChangeType={this.state.filters.type}
+              onFindPetsClick={this.handleFetch}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser 
+              onAdoptPet={this.isAdoptedFunct}
+              />
             </div>
           </div>
         </div>
