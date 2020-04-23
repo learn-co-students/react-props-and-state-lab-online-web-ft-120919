d887a0e319981a39a63d342023008cc5085c26ee
github username <meliajm@gmail.com>
2020-04-23 11:13:31 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -16,33 +16,79 @@ class App extends React.Component {
   }
 
 
-  // not sure about this part, how to set state.pets to result of fetch???
-  // setPetsFromFetch = () => {
-  //   this.setState({
-  //     pets: handleFetch()
-  //   })
-  // }
+  
 
 
   handleFetch = () => {
     if (this.state.filters.type === 'cat'){
-      fetch('/api/pets?type=cat')
+      let url = '/api/pets?type=cat'
+      fetch(url)
+    .then(resp=>resp.json())
+    .then(jsonData=>
+      this.setState({
+        pets: jsonData
+      })
+      )
+      // fetch('/api/pets?type=cat')
     } else if (this.state.filters.type === 'dog') {
-      fetch('/api/pets?type=dog')
+      // fetch('/api/pets?type=dog')
+      let url = '/api/pets?type=dog'
+      fetch(url)
+    .then(resp=>resp.json())
+    .then(jsonData=>
+      this.setState({
+        pets: jsonData
+      })
+      )
     } else if (this.state.filters.type === 'micropig') {
-      fetch('/api/pets?type=micropig')
+      // fetch('/api/pets?type=micropig')
+      let url = '/api/pets?type=micropig'
+      fetch(url)
+    .then(resp=>resp.json())
+    .then(jsonData=>
+      this.setState({
+        pets: jsonData
+      })
+      )
     } else {
-      fetch('/api/pets')
+      let url = '/api/pets'
+      fetch(url)
+    .then(resp=>resp.json())
+    .then(jsonData=>
+      
+      this.setState({
+        pets: jsonData
+        
+      })
+      )
+      // fetch('/api/pets')
     }
+    
   }
 
-  // need to find first not just check equality for each pets????
   isAdoptedFunct = (petId) => {
-    if (this.state.pets.id===petId) {
-      this.state.pets.isAdopted=true
-    }
+    
+    const newState = this.state.pets.map( pet => {
+      if (pet.id === petId) {
+        pet.isAdopted = true
+      }
+      return pet 
+    })
+    this.setState({
+      pets: newState
+    })
+    
+  }
+  // console.log(this.state.pets)
+  // return this.state.pets
 
-
+  handleOnChangeType = (event) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: event.target.value 
+      }
+    })
   }
 
   render() {
@@ -55,7 +101,7 @@ class App extends React.Component {
           <div className="ui grid">
             <div className="four wide column">
               <Filters 
-              onChangeType={this.state.filters.type}
+              onChangeType={this.handleOnChangeType}
               onFindPetsClick={this.handleFetch}
               />
             </div>
