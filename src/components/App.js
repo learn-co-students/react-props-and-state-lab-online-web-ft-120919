6fa9c9a9ac62ec7a1282a9af1c254af1e62fad50
ben20262 <ben20262@aim.com>
2020-04-24 17:39:40 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -4,36 +4,69 @@ import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
 class App extends React.Component {
-  constructor() {
-    super()
+	constructor() {
+		super()
 
-    this.state = {
-      pets: [],
-      filters: {
-        type: 'all'
-      }
-    }
+		this.state = {
+			pets: [],
+			filters: {
+			type: 'all'
+			}
+		}
+	}
+
+  onChangeType = type => {
+	this.setState({
+		filters: {
+			type
+		}
+	})
   }
 
-  render() {
-    return (
-      <div className="ui container">
-        <header>
-          <h1 className="ui dividing header">React Animal Shelter</h1>
-        </header>
-        <div className="ui container">
-          <div className="ui grid">
-            <div className="four wide column">
-              <Filters />
-            </div>
-            <div className="twelve wide column">
-              <PetBrowser />
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+	onFindPetsClick = () => {
+		let petURL = '/api/pets'
+		if (this.state.filters.type !== 'all') {
+			petURL = `/api/pets?type=${this.state.filters.type}`
+		}
+		fetch(petURL)
+		.then(resp => resp.json())
+		.then(pets => this.setState({
+			pets
+		}))
+	}
+
+	onAdoptPet = (petID) => {
+		console.log(petID)
+		let petArray = this.state.pets.map(pet => {
+			if (pet.id === petID) {
+				pet.isAdopted = true
+			}
+			return pet
+		})
+		this.setState({
+			pets: petArray
+		})
+	}
+
+	render() {
+		return (
+			<div className="ui container">
+			<header>
+				<h1 className="ui dividing header">React Animal Shelter</h1>
+			</header>
+			<div className="ui container">
+				<div className="ui grid">
+				<div className="four wide column">
+					<Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
+				</div>
+				<div className="twelve wide column">
+					<PetBrowser onAdoptPet={this.onAdoptPet} pets={this.state.pets} />
+				</div>
+				</div>
+			</div>
+			</div>
+		)
+	}
 }
 
 export default App
