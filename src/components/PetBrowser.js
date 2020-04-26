3ed740ad954adc4fe60af5a646fbc6008e6eb436
allison_johnson <allison.cuttler@gmail.com>
2020-04-25 19:00:57 -0700

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -6,15 +6,20 @@ class PetBrowser extends React.Component {
   render() {
     return this.props.pets.map(petInfo => {
       return (
-        <Pet name={petInfo.name} 
-          type={petInfo.type}
-          age={petInfo.age}
-          weight={petInfo.weight}
-          gender={petInfo.gender}
+        <Pet 
+          pet={petInfo} 
+          onAdoptPet={this.props.onAdoptPet}
         />
+        // <Pet name={petInfo.name} 
+        //   type={petInfo.type}
+        //   age={petInfo.age}
+        //   weight={petInfo.weight}
+        //   gender={petInfo.gender}
+        //   onAdoptPet={this.props.onAdoptPet}
+        // />
       )//return
     })//map
   }//render
-}
+}//class
 
 export default PetBrowser
