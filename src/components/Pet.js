5456b2ac619b9da4a1763f939ef13a907e1393b1
rick <github email address>
2020-04-26 21:36:31 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,29 +1,35 @@
-import React from 'react'
+import React from "react";
 
 class Pet extends React.Component {
+  handleAdopt = (e)=>{
+    this.props.onAdoptPet(e.target.id)
+    e.target.className= `ui disabled button`
+    e.target.previousElementSibling.className='ui green button'
+
+  }
   render() {
     return (
       <div className="card">
         <div className="content">
           <a className="header">
             {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date"> {this.props.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.age}</p>
+            <p>Weight: {this.props.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          <button onChange={this.enable} className="ui disabled button">Already adopted</button>
+          <button id = {this.props.id} onClick={this.handleAdopt} className="ui primary button">Adopt pet</button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Pet
+export default Pet;
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import Pet from './Pet'
+import Pet from "./Pet";
 
 class PetBrowser extends React.Component {
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    console.log(this.props);
+    return (
+      <div className="ui cards">
+        {this.props.pets.map((pet) => {
+          return (
+            <Pet
+              onAdoptPet={this.props.onAdoptPet}
+              type={pet.type}
+              name={pet.name}
+              age={pet.age}
+              weight={pet.weight}
+              id={pet.id}
+            />
+          );
+        })}
+      </div>
+    );
   }
 }
 
-export default PetBrowser
+export default PetBrowser;
